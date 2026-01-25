0f4724f68ced014c8e5433b9185977d9d1c24627
ahmadoudiallo <adial39@wgu.edu>
2026-01-24 20:42:09 -0500

    Adding step 4 data splitting

--- main.py
+++ main.py
@@ -105,11 +105,25 @@ def go(config: DictConfig):
             ##################
             pass
         """
-        if "data_split" in active_steps:
+        if "data_split" in active_steps:     
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                },
+            )
+
+            
+
             ##################
             # Implement here #
             ##################
-            pass
+            #pass
 
         if "train_random_forest" in active_steps:
 
